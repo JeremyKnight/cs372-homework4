cee4c080fd2ab9493a72e5cea6d62776e5210237
jeremyknight <jeremyknightjck99@gmail.com>
2019-01-24 20:14:46 -0800

    PR2_2 done for the most part

---
+++ PR2_2/Spider.java
@@ -0,0 +1,136 @@
+import java.util.regex.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+
+public class Spider implements Runnable {
+
+    String regexUrl = "<\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]>"; // matches <http://google.com>
+    String regexEmail = "\"mailto:(.*?)\"";
+    String url;
+    Map<String, Boolean> websiteMap = new ConcurrentHashMap<String, Boolean>();
+    ArrayList<String> emails = new ArrayList<String>();
+
+    public Spider(String url) {
+       this.url = url;
+
+    }
+
+    public void run() {
+        addWebsites(url);
+        findEmail(url);
+
+        System.out.println("done");
+        //for(Entry<String, Boolean> entry : websiteMap.entrySet()) {
+        for(String key : websiteMap.keySet()) {
+    
+            System.out.println(key);
+            if(websiteMap.get(key)==false) {
+                //String web = entry.getKey();
+
+                addWebsites(key);
+                findEmail(key);
+                //entry.setValue(true);
+                System.out.println(Integer.toString(emails.size()));
+            }
+            
+
+            if(emails.size()>=100) {
+                break;
+            }
+        }
+
+        for(int i=0; i<emails.size(); i++) {
+            System.out.println(emails.get(i));
+        }
+       
+    }
+
+    private void addWebsites(String _url) {
+        URL u;
+        try {
+            u = new URL(_url);
+
+        } catch(Exception ex) {
+            System.out.println("this failed " + ex.getMessage());
+            u = null;
+        }
+
+        if(url == null) return;
+        try {
+            BufferedReader rdr = new BufferedReader(new InputStreamReader(u.openStream()));
+            String line ="";
+            while((line = rdr.readLine()) != null ) {
+
+                //Pattern pattern = Pattern.compile(regexUrl);
+                //String newThing = "";
+                
+                if(line.contains("https://") || line.contains("href=\"/")) {
+                    int lineNum = 0;
+
+                    for(int i = 0; i<line.length(); i++) {
+                        if((line.charAt(i) == '\"') && lineNum == 0) {
+                            lineNum = i;
+                        } else if((line.charAt(i) == '\"') && lineNum != 0) {
+                            String newUrl = line.substring(lineNum+1, i);
+                            if(newUrl.contains("https://") ) { 
+                                //System.out.println(newUrl);
+                                if(newUrl.endsWith("js") || newUrl.endsWith("html")) {
+                                } else {
+                                    if(!websiteMap.containsKey(newUrl))
+                                        websiteMap.put(newUrl, false);
+                                    //System.out.println(newUrl + " from: " + _url);
+                                }
+                            } else if(newUrl.contains("\"/")) {
+                                
+                                System.out.println("added url:" + _url + newUrl);
+                                //website.put()
+                            } 
+                            //System.out.println(newUrl);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        } catch(Exception ex) {
+            System.out.println("this failed " + ex.getMessage());
+        }
+    }
+
+    private void findEmail(String s) {
+        //href="mailto:
+        URL u;
+        try {
+            u = new URL(s);
+        } catch(Exception e) {
+            u = null;
+            System.out.println("this failed " + e.getMessage());
+        }
+
+        try {
+            Pattern email = Pattern.compile(regexEmail);
+            BufferedReader rdr = new BufferedReader(new InputStreamReader(u.openStream()));
+            String line ="";
+            while((line = rdr.readLine()) != null ) {
+                Matcher match = email.matcher(line);
+                if(match.find() && match.group(1).length()<100) { //&& !line.contains("-")) {
+                    emails.add(match.group(1));
+                    System.out.println("email: " + match.group(1));
+                }
+            }
+
+
+        } catch(Exception e) {
+            System.out.println("this failed in find email " + e.getMessage());
+        }
+
+
+
+    }
+}
---
+++ PR2_2/TestSpider.java
@@ -0,0 +1,19 @@
+public class TestSpider {
+    public static void main(String[] args) {
+        //String site = "https://stackoverflow.com/questions/43015098/how-to-iterate-through-a-map-in-java/43015629";
+        String site = "https://www.whitworth.edu/cms/";
+        //String site = "https://www.whitworth.edu/cms/administration/university-communications/staff/";
+        //String site = "https://www.procore.com/";
+
+        Spider spider = new Spider(site);
+
+        Thread spiderThread = new Thread(spider);
+        spiderThread.start();
+
+        try {
+            spiderThread.sleep(100);
+        } catch(Exception ex) {
+            System.out.println("this failed " + ex.getMessage());
+        }
+    }
+}
